99aef4c521a9450a5590b581773b809e9e0d0ac0
Alex Bates <alexlbates@gmail.com>
2021-07-16 10:34:32 -0400

    add back in private subnets. configure nat gateway with routing rules to facilitate this. shift ec2 scaling instances into the private subnet. Meaning - the only thing in the public subnet should be the nat gateway + load balancer

--- ecs-full-setup-example/networking.tf
+++ ecs-full-setup-example/networking.tf
@@ -1,3 +1,4 @@
+#region -- public-network-setup
 resource "aws_internet_gateway" "testing_internet_gateway" {
   # Internet gateway is required to allow traffic to the outside world from the vpc.
   vpc_id = aws_vpc.testing_vpc.id
@@ -7,13 +8,11 @@ resource "aws_internet_gateway" "testing_internet_gateway" {
 resource "aws_subnet" "public_subnets" {
   cidr_block = element(var.public-subnets, count.index)
   vpc_id     = aws_vpc.testing_vpc.id
-  tags       = { Name = "${var.environment-name}-public-subnet-${count.index}" }
+  tags       = { Name = "${var.environment-name}-public-subnet" }
   count      = length(var.public-subnets)
 }
 
 resource "aws_route_table" "public_route_table" {
-  # this seems to be very slow to execute in terraform initially? either that or it just breaks outright and is fixed on the next pass through...
-  # should look into it more. a quick google didnt do much good.
   vpc_id = aws_vpc.testing_vpc.id
   route {
     cidr_block = "0.0.0.0/0" # catch all traffic
@@ -27,6 +26,8 @@ resource "aws_route_table_association" "public_route_table_association" {
   route_table_id = aws_route_table.public_route_table.id
   subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
   count          = length(var.public-subnets)
+
+  depends_on = [aws_route_table.public_route_table, aws_subnet.public_subnets]
 }
 
 resource "aws_security_group" "load_balancer_security_group" {
@@ -61,9 +62,9 @@ resource "aws_alb" "load_balancer" {
 
 resource "aws_alb_target_group" "lb_target_group" {
   name = "${var.environment-name}-target-group"
-  port = 80
+  port = 80 # port that receives traffic. So, *incoming* on port 80.
   protocol = "HTTP"
-  target_type = "instance"
+  target_type = "instance" # targets are specified by instance ID.
   vpc_id = aws_vpc.testing_vpc.id
 
   health_check {
@@ -87,3 +88,59 @@ resource "aws_lb_listener" "listener" {
     target_group_arn = aws_alb_target_group.lb_target_group.id
   }
 }
+#endregion public-network-setup
+
+#region private-network-setup
+resource "aws_subnet" "private_subnets" {
+  cidr_block = element(var.private-subnets, count.index)
+  vpc_id     = aws_vpc.testing_vpc.id
+  tags       = { Name = "${var.environment-name}-private-subnet" }
+  count      = length(var.public-subnets)
+}
+
+resource "aws_eip" "nat-gateway-eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat-gateway-eip.id
+  subnet_id = aws_subnet.public_subnets[0].id # im hoping i can get away with just the one gateway for however many private subnets...
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.testing_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0" # catch all traffic
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = { Name = "${var.environment-name}-route-table" }
+}
+
+resource "aws_route_table_association" "private_route_table_association" {
+  # connect the route table (catch all traffic) to the public subnet.
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  count          = length(var.private-subnets)
+}
+
+resource "aws_security_group" "private_security_group" {
+  vpc_id = aws_vpc.testing_vpc.id
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    security_groups = [aws_security_group.load_balancer_security_group.id]
+  }
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = { Name = "${var.environment-name}-lb-security-group" }
+}
+#endregion private-network-setup
