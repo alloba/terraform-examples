601cf53ca88872c05fb91f73119f223689c0fd2f
Alex Bates <alexlbates@gmail.com>
2021-07-29 10:41:25 -0400

    add more outputs to the vpc module

--- modules/private-cloud/outputs.tf
+++ modules/private-cloud/outputs.tf
@@ -2,14 +2,26 @@ output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
-output "public_subnet_id" {
+output "public_subnet_ids" {
   value = aws_subnet.public-subnets[*].id
 }
 
-output "private_subnet_id" {
+output "private_subnet_ids" {
   value = aws_subnet.private-subnets[*].id
 }
 
 output "eip_addresses" {
   value = aws_eip.nat[*].public_ip
+}
+
+output "security_group_egress_all_id" {
+  value = aws_security_group.egress-all.id
+}
+
+output "security_group_ingress_http_id" {
+  value = aws_security_group.http.id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw[0].id # there is only ever one, if networking is enabled. if not, this will probably break...
 }
